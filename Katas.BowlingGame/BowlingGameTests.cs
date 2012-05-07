167841199df19be90ddabe28fd00a3f7aa34a068
Bradley Ford <code@bradleyford.id.au>
2012-05-07 21:00:29 +1000

    First attempt

--- Katas.BowlingGame/BowlingGameTests.cs
+++ Katas.BowlingGame/BowlingGameTests.cs
@@ -6,5 +6,79 @@ namespace Katas.BowlingGame
 	[TestFixture]
 	public class BowlingGameTests
 	{
+		private Game _game;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_game = new Game();
+		}
+
+		private void RollStrike()
+		{
+			_game.Roll(10);
+		}
+
+		private void RollSpare()
+		{
+			_game.Roll(5);
+			_game.Roll(5);
+		}
+
+		private void RollMany(int rolls, int pins)
+		{
+			for (var i = 0; i < rolls; i++)
+			{
+				_game.Roll(pins);
+			}
+		}
+
+		[Test]
+		public void TestGutterGame()
+		{
+			RollMany(20, 0);
+
+			Assert.AreEqual(0, _game.Score);
+		}
+
+
+		[Test]
+		public void TestAllOnes()
+		{
+			RollMany(20, 1);
+
+			Assert.AreEqual(20, _game.Score);
+		}
+
+		[Test]
+		public void TestOneSpare()
+		{
+			RollSpare();
+			_game.Roll(3);
+			
+			RollMany(17, 0);
+
+			Assert.AreEqual(16, _game.Score);
+		}
+
+		[Test]
+		public void TestOneStrike()
+		{
+			RollStrike();
+			_game.Roll(3);
+			_game.Roll(4);
+
+			RollMany(16, 0);
+
+			Assert.AreEqual(24, _game.Score);
+		}
+
+		[Test]
+		public void TestPerfectGame()
+		{
+			RollMany(12, 10);
+
+			Assert.AreEqual(300, _game.Score);
+		}
 	}
 }
---
+++ Katas.BowlingGame/Game.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Katas.BowlingGame
+{
+	public class Game
+	{
+		private int[] _rolls = new int[21];
+		private int _rollIndex;
+
+		public void Roll(int pins)
+		{
+			_rolls[_rollIndex++] = pins;
+		}
+
+		public int Score
+		{
+			get
+			{
+				var score = 0;
+				var frameIndex = 0;
+
+				for (var frame = 0; frame < 10; frame++)
+				{
+					if (IsStrike(frameIndex))
+					{
+						score += 10 + GetStrikeBonus(frameIndex);
+
+						frameIndex++;
+					}
+					else if (IsSpare(frameIndex))
+					{
+						score += 10 + GetSpareBonus(frameIndex);
+						frameIndex += 2;
+					}
+					else
+					{
+						score += GetFrameTotal(frameIndex);
+						frameIndex += 2;
+					}
+				}
+					
+				return score;
+			}
+		}
+
+		private bool IsStrike(int frameIndex)
+		{
+			return _rolls[frameIndex] == 10;
+		}
+
+		private bool IsSpare(int frameIndex)
+		{
+			return GetFrameTotal(frameIndex) == 10;
+		}
+
+		private int GetStrikeBonus(int frameIndex)
+		{
+			return GetFrameTotal(frameIndex + 1);
+		}
+
+		private int GetSpareBonus(int frameIndex)
+		{
+			return _rolls[frameIndex + 2];
+		}
+
+		private int GetFrameTotal(int frameIndex)
+		{
+			return _rolls[frameIndex] + _rolls[frameIndex + 1];
+		}
+	}
+}
