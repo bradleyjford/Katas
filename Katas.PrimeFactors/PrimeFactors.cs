21a6976a5a148dc1fb3b5dd6e48736676f9c670a
Bradley Ford <code@bradleyford.id.au>
2012-05-13 10:50:48 +1000

    First attempt at PrimeFactors Kata

---
+++ Katas.PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Katas.PrimeFactors
+{
+	public static class PrimeFactors
+	{
+		public static int[] Generate(int n)
+		{
+			var factors = new List<int>();
+
+			for (var candidate = 2; n > 1; candidate++)
+			{
+				for (; n % candidate == 0; n /= candidate )
+				{
+					factors.Add(candidate);
+				}
+			}
+
+			return factors.ToArray();
+		} 
+	}
+}
--- Katas.PrimeFactors/PrimeFactorsTest.cs
+++ Katas.PrimeFactors/PrimeFactorsTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Katas.PrimeFactors
@@ -6,6 +8,68 @@ namespace Katas.PrimeFactors
 	[TestFixture]
 	public class PrimeFactorsTest
 	{
+		[Test]
+		public void Test1()
+		{
+			var factors = PrimeFactors.Generate(1);
 
+			Assert.AreEqual(0, factors.Length);
+		}
+
+		[Test]
+		public void Test2()
+		{
+			var factors = PrimeFactors.Generate(2);
+
+			Assert.AreEqual(new [] { 2 }, factors);
+		}
+
+		[Test]
+		public void Test3()
+		{
+			var factors = PrimeFactors.Generate(3);
+
+			Assert.AreEqual(new [] { 3 }, factors);
+		}
+
+		[Test]
+		public void Test4()
+		{
+			var factors = PrimeFactors.Generate(4);
+
+			Assert.AreEqual(new [] { 2, 2 }, factors);
+		}
+
+		[Test]
+		public void Test6()
+		{
+			var factors = PrimeFactors.Generate(6);
+
+			Assert.AreEqual(new [] { 2, 3 }, factors);
+		}
+
+		[Test]
+		public void Test8()
+		{
+			var factors = PrimeFactors.Generate(8);
+
+			Assert.AreEqual(new [] { 2, 2, 2 }, factors);
+		}
+
+		[Test]
+		public void Test9()
+		{
+			var factors = PrimeFactors.Generate(9);
+
+			Assert.AreEqual(new [] { 3, 3 }, factors);
+		}
+
+		[Test]
+		public void Test12()
+		{
+			var factors = PrimeFactors.Generate(12);
+
+			Assert.AreEqual(new [] { 2, 2, 3 }, factors);
+		}
 	}
 }
